4a7161bee4a02b083fadae8756d65525aced58e0
ayue19960816 <uceslim@ucl.ac.uk>
2019-03-06 11:50:40 -0500

    find the closest point and show the question pop-up automatically

--- uceslim/www/js/getFormData4.js
+++ uceslim/www/js/getFormData4.js
@@ -75,3 +75,51 @@ mymap.closePopup();
 // the answerSelected variable holds the number of the answer
 //that the user picked
 }
+
+//find the closest point and show the question pop-up automatically
+function closestFormPoint() {
+// take the leaflet formdata layer
+// go through each point one by one
+// and measure the distance to Warren Street
+// for the closest point show the pop up of that point
+var minDistance = 100000000000;
+var closestFormPoint = 0;
+// for this example, use the latitude/longitude of warren street
+// in your assignment replace this with the user's location
+var userlat = 51.524048;
+var userlng = -0.139924;
+formLayer.eachLayer(function(layer) {
+var distance = calculateDistance(userlat, userlng,layer.getLatLng().lat, layer.getLatLng().lng, 'K');
+if (distance < minDistance){
+minDistance = distance;
+closestFormPoint = layer.feature.properties.id;
+}
+});
+// for this to be a proximity alert, the minDistance must be
+// closer than a given distance - you can check that here
+// using an if statement
+// show the popup for the closest point
+formLayer.eachLayer(function(layer) {
+if (layer.feature.properties.id == closestFormPoint){
+layer.openPopup();
+}
+});
+}
+
+// code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-inyour-web-apps.html
+function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+var radlat1 = Math.PI * lat1/180;
+var radlat2 = Math.PI * lat2/180;
+var radlon1 = Math.PI * lon1/180;
+var radlon2 = Math.PI * lon2/180;
+var theta = lon1-lon2;
+var radtheta = Math.PI * theta/180;
+var subAngle = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+subAngle = Math.acos(subAngle);
+subAngle = subAngle * 180/Math.PI; // convert the degree value returned by acos back to degrees from radians
+dist = (subAngle/360) * 2 * Math.PI * 3956; // ((subtended angle in degrees)/360) * 2 * pi * radius )
+// where radius of the earth is 3956 miles
+if (unit=="K") { dist = dist * 1.609344 ;} // convert miles to km
+if (unit=="N") { dist = dist * 0.8684 ;} // convert miles to nautical miles
+return dist;
+}
